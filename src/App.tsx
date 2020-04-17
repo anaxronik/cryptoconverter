7c971f02474baec48efade9958798e4b0e4a1d3f
Sergey Komarov <anaxronik@gmail.com>
2020-04-17 17:05:53 +0500

    Create Main and right block

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import RightBlock from './components/RightBlock';
+import MainBlock from './components/MainBlock';
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      asdsd
+    <Container maxWidth="lg" className='container'>
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Paper className='paper' elevation={3}>
+            <MainBlock />
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className='paper' elevation={3}>
+            <RightBlock />
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
---
+++ src/components/RightBlock.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+
+
+export default function Right() {
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    )
+}

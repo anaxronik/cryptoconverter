e1e309d59318c990bbb51597e99825bb373cc360
Sergey Komarov <anaxronik@gmail.com>
2020-04-17 16:46:06 +0500

    install materiaal ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
-    <div className="App">
-      work
-    </div>
+    <Container maxWidth="lg">
+      asdsd
+    </Container>
   );
 }
 

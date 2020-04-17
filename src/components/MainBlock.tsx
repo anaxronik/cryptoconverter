ac1572915885c32cfa671ec1fac227d4a9f76efc
Sergey Komarov <anaxronik@gmail.com>
2020-04-17 17:50:47 +0500

    create blocks contetnt

--- src/components/MainBlock.tsx
+++ src/components/MainBlock.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-export default function Main() {
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function MainBlock() {
+
     return (
-        <div>
-            Maaaain
-        </div>
-    )
+        <TableContainer component={Paper}>
+            <Table className='' aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell> </TableCell>
+                        <TableCell align="left">FullName</TableCell>
+                        <TableCell align="left">Name</TableCell>
+                        <TableCell align="left">Price</TableCell>
+                        <TableCell align="left">Volume24h</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="left">{row.calories}</TableCell>
+                            <TableCell align="left">{row.fat}</TableCell>
+                            <TableCell align="left">{row.carbs}</TableCell>
+                            <TableCell align="left">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
--- src/components/RightBlock.tsx
+++ src/components/RightBlock.tsx
@@ -1,12 +1,62 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
 
 
 export default function Right() {
+    const onChange = () => {
+        console.log('onChange')
+    }
+
     return (
         <div>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </div>
+            <div className='curencyBlock'>
+                <TextField className='TextField' id="outlined-basic" label="Отдам" variant="outlined" />
+
+                <FormControl variant="outlined" className='FormControl'>
+                    <InputLabel htmlFor="outlined-age-native-simple">$</InputLabel>
+                    <Select
+                        native
+                        value={[]}
+                        onChange={onChange}
+                        label="Age"
+                        inputProps={{
+                            name: 'age',
+                            id: 'outlined-age-native-simple',
+                        }}
+                    >
+                        <option aria-label="None" value="" />
+                        <option value={10}>Ten</option>
+                        <option value={20}>Twenty</option>
+                        <option value={30}>Thirty</option>
+                    </Select>
+                </FormControl>
+            </div>
+
+            <div className='curencyBlock'>
+                <TextField className='TextField' id="outlined-basic" label="Получу" variant="outlined" />
+                <FormControl variant="outlined" className='FormControl'>
+                    <InputLabel htmlFor="outlined-age-native-simple">$</InputLabel>
+                    <Select
+                        native
+                        value={[]}
+                        onChange={onChange}
+                        label="Age"
+                        inputProps={{
+                            name: 'age',
+                            id: 'outlined-age-native-simple',
+                        }}
+                    >
+                        <option aria-label="None" value="" />
+                        <option value={10}>Ten</option>
+                        <option value={20}>Twenty</option>
+                        <option value={30}>Thirty</option>
+                    </Select>
+                </FormControl>
+            </div>
+
+        </div >
     )
 }
